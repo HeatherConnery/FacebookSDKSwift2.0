691c97ead495514e2f6f8afb58106ac7f0125d00
HeatherConnery <heatherconnery@gmail.com>
2015-11-24 11:57:10 -0500

    commit after pod set up

---
+++ FacebookSDK/FacebookSDK/ObjectiveC-Bridge.h
@@ -0,0 +1,13 @@
+//
+//  ObjectiveC-Bridge.h
+//  FacebookSDK
+//
+//  Created by Heather Connery on 2015-11-24.
+//  Copyright © 2015 HConnery. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjectiveC_Bridge : NSObject
+
+@end
---
+++ FacebookSDK/FacebookSDK/ObjectiveC-Bridge.m
@@ -0,0 +1,13 @@
+//
+//  ObjectiveC-Bridge.m
+//  FacebookSDK
+//
+//  Created by Heather Connery on 2015-11-24.
+//  Copyright © 2015 HConnery. All rights reserved.
+//
+
+#import "ObjectiveC-Bridge.h"
+
+@implementation ObjectiveC_Bridge
+
+@end
