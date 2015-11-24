5410230583e1c887c94c737701f0ee29033ac626
HeatherConnery <heatherconnery@gmail.com>
2015-11-24 10:45:14 -0500

    initial set up

---
+++ FacebookSDKSwift2.0/FacebookSDKSwift2.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FacebookSDKSwift2.0
+//
+//  Created by Heather Connery on 2015-11-24.
+//  Copyright © 2015 HConnery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
